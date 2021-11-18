c9b98a81155d1b286a59b4b1fab0ae5bd27d289c
KazukiTerashima <r0o9t1r3ba.kzk@gmail.com>
2021-11-18 11:19:16 +0900

    improve ms2pdf.py about split module

--- ms2pdf.py
+++ ms2pdf.py
@@ -17,43 +17,76 @@ def excel2pdf(input_file, output_file):
     #エクセルを閉じる
     app.Quit()
 
-if __name__ == '__main__':
-    print("Which dir(full path)?:", end="")
-    # 対象フォルダ
-    input_dir = (input()+"\\").replace('/', os.sep)
-    filenames = os.listdir(input_dir)
-    output_dir = (input_dir + "pdf/").replace('/', os.sep)
-    # ディレクトリが存在しない場合、ディレクトリを作成する
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    for file in filenames:
-        word_match = re.search("\.docx$", file) 
-        if word_match: 
-            docx2pdf.convert(input_dir+file, output_dir+file[:-5]+".pdf")
-            print(file)
-        excel_match = re.search("\.xlsx$", file) 
-        if excel_match: 
-            excel2pdf(input_dir+file, output_dir+file[:-5]+".pdf")
-            print(file)
+def pdf_merger(m_out = ""):
+    if not m_out:
+        print("対象ディレクトリを入力:", end="")
+        m_out = (input()+"\\").replace('/', os.sep)
     print()
     print("-------------------------------")
-    print("merging PDF from:" + output_dir)
-    print("↓↓↓↓↓↓↓↓↓↓↓↓ target file ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓")
+    print("merging PDF from:" + m_out)
+    print("※※※※※※ ファイルの順番と数を確認してください ※※※※※※")
     merger = PyPDF2.PdfFileMerger()
 
-    filenames = os.listdir(output_dir) 
+    filenames = os.listdir(m_out) 
     num = 0
     for file in filenames: 
-            merger.append(output_dir + file)
-            print(file)
-            num += 1
-    print("↑↑↑↑↑↑↑↑↑↑↑↑ target file ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑")
+        merger.append(m_out + file)
+        print(file)
+        num += 1
+    print("※※※※※※ ファイルの順番と数を確認してください ※※※※※※")
     print("total: " + str(num) + "file")
-    print()
     print("-------------------------------")
-    print("What merged file name?:", end="")
+    print()
+    print("結合したPDFの名前を入力:", end="")
     merged_file_name = input()
     if merged_file_name[-4:] != ".pdf":
             merged_file_name += ".pdf"
-    merger.write(output_dir + merged_file_name)
+    merger.write(m_out + merged_file_name)
     merger.close()
+
+if __name__ == '__main__':
+    ptn = 0
+    print("excelファイルとwordファイルをPDF化して全てを結合します。")
+    print("PDF化のみ：\"1\"、PDF結合のみ：\"2\",すべて実行する：入力なし、でEnterキーを押してください")
+    ptn = int(input())
+    if ptn in [0, 1, 2]:
+        if ptn == 0 or ptn == 1:
+            print("対象ディレクトリを入力:", end="")
+            # 対象フォルダ
+            input_dir = (input()+"\\").replace('/', os.sep)
+            filenames = os.listdir(input_dir)
+            output_dir = (input_dir + "pdf/").replace('/', os.sep)
+            # ディレクトリが存在しない場合、ディレクトリを作成する
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            for file in filenames:
+                word_match = re.search("\.docx$", file) 
+                if word_match: 
+                    docx2pdf.convert(input_dir+file, output_dir+file[:-5]+".pdf")
+                    print(file)
+                excel_match = re.search("\.xlsx$", file) 
+                if excel_match: 
+                    excel2pdf(input_dir+file, output_dir+file[:-5]+".pdf")
+                    print(file)
+            if ptn == 1:
+                print("-------------------------------")
+                print("merging PDF from:" + output_dir)
+                print("※※※※※※ ファイルの順番と数を確認してください ※※※※※※")
+
+                filenames = os.listdir(output_dir) 
+                num = 0
+                for file in filenames: 
+                    print(file)
+                print("※※※※※※ ファイルの順番と数を確認してください ※※※※※※")
+                print("total: " + str(num) + "file")
+                print("-------------------------------")
+                print()
+                print("結合したPDFの名前を入力:", end="")
+                print("正常に処理が終了しました。")
+
+        if ptn == 0 or ptn == 2:
+            pdf_merger()
+            print("正常に処理が終了しました。")
+    else:
+        print("入力が不正です。")
+
